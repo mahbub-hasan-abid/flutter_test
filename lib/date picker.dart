e0c9b34aa63bc2cd673285d978e8e96f42e6d2e8
mahbub-hasan-abid <mahbubhasanabid00@gmail.com>
2023-10-22 13:31:15 +0600

    4

---
+++ lib/date picker.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  return runApp(dateapp());
+}
+
+class dateapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "date app 2",
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: datepage(),
+    );
+  }
+}
+
+class datepage extends StatefulWidget {
+  @override
+  State<datepage> createState() => _datepageState();
+}
+
+//test
+class _datepageState extends State<datepage> {
+  @override
+  Widget build(BuildContext context) {
+    var time = DateTime.now();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Date picker",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "select date",
+            style: TextStyle(fontSize: 25),
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                DateTime? datepic = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2023),
+                    lastDate: DateTime(2025));
+                if (datepic != null) {
+                  print('date picked : ${datepic.day}');
+                }
+              },
+              child: Text("show"))
+        ],
+      ),
+    );
+  }
+}
