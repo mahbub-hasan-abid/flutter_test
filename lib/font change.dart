02886f7e61058ba8f53000c584ad220fe14e6fa0
mahbub-hasan-abid <mahbubhasanabid00@gmail.com>
2023-11-15 21:36:23 +0600

    font change frequently

---
+++ lib/font change.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(font_change());
+}
+
+class font_change extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
