43087bedcc2066f60fdf874d55c3ee0a98dd7a5c
mahbub-hasan-abid <mahbubhasanabid00@gmail.com>
2023-11-23 02:27:26 +0600

    switching data  from one screen to another

--- lib/passing data from one page to another/main.dart
+++ lib/passing data from one page to another/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/passing%20data%20from%20one%20page%20to%20another/second_page.dart';
 
 void main() {
   return runApp(PassData());
@@ -11,9 +12,79 @@ class PassData extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App',
-      theme: ThemeData(primarySwatch: Colors.yellow),
-      home: PassDataFirstScreen() , 
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: PassDataFirstScreen(),
     );
   }
 }
-st
+
+class PassDataFirstScreen extends StatefulWidget {
+  PassDataFirstScreen({super.key});
+
+  @override
+  State<PassDataFirstScreen> createState() => _PassDataFirstScreenState();
+}
+
+class _PassDataFirstScreenState extends State<PassDataFirstScreen> {
+  var NameController = TextEditingController();
+  GlobalKey<FormState> formK = GlobalKey<FormState>();
+  var result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Enter your name:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 200,
+              // height: 40,
+              child: Form(
+                key: formK,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'enter name';
+                    }
+                    return null;
+                  },
+                  controller: NameController,
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (formK.currentState!.validate()) {
+                    result = NameController.text;
+                    setState(() {});
+                  }
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondPageOfDataPass(result),
+                      ));
+                },
+                child: Text(
+                  'Log in !',
+                  style: TextStyle(fontSize: 16),
+                )),
+            // Text(result.toString().isEmpty ? 'your name' : result)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/passing data from one page to another/second_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SecondPageOfDataPass extends StatelessWidget {
+  var name;
+
+  SecondPageOfDataPass(this.name);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('secondpage'),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          color: Colors.amber,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              'Welcome \n $name',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: const Color.fromARGB(255, 255, 255, 255)),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
