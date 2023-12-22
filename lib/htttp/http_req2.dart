c65f776467a4ff7af83afa368b7a70ce6b635b61
mahbub-hasan-abid <mahbubhasanabid00@gmail.com>
2023-12-22 17:48:03 +0600

    json data from http req

---
+++ lib/htttp/http_req2.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var data;
+  Future fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    setState(() {
+      var decode = json.decode(response.body);
+      data = decode;
+      print(data);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.fetchData();
+    print('ok');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Flutter HTTP Request Example'),
+      ),
+      body: data == null
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return Column(children: [
+                  Card(
+                    color: Color.fromARGB(255, 213, 223, 226),
+                    child: ListTile(
+                      leading:
+                          CircleAvatar(child: Text('${data[index]['id']}')),
+                      title: Text(data[index]['title']),
+                      trailing: index % 2 == 0
+                          ? Icon(Icons.emoji_people_outlined)
+                          : Icon(Icons.ac_unit_outlined),
+                    ),
+                  ),
+                ]);
+              },
+            ),
+    );
+  }
+}
